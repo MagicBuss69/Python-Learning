98a1444c0fc58ef7c1e7f484bae2918e9f1d8818
MagicBuss69 <jarik1060@gmail.com>
2026-02-19 19:10:58 +0100

    Added first lines

---
+++ day1py.py
@@ -0,0 +1,6 @@
+print("Hello, world!")
+name = "Jaroslav"
+age = 20
+print("My name is", name, "and I am", age, "years old.")
+if age > 18
+print ("You are an adult")
---
+++ day2py.py
@@ -0,0 +1,12 @@
+# Day 2 input practice
+name = input("Enter your name: ")
+print("Hello,", name, "!")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print("The sum is:", num1 + num2)
+number = int(input("Enter favorite number: "))
+print("Your number plus 10 is:", number + 10)
+
+name = input("Enter your name: ")
+age = int(input("What is your age:"))   
+print(f"In 10 years, {name} will be {age + 10} years old.")
